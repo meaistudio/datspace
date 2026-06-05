644a22cf3b98c2f41c10101c6ead313c1a49c3bc
meaistudio <studiocreative.meai@gmail.com>
2026-06-05 09:12:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ const DOCUMENTATION_ARCHIVE = {
         date: "April 4, 2026",
         location: "MEAI Studio",
         manifesto: "A visual artist meeting space to share work, processes, and ideas through artist talks, work reviews, and open discussions.",
-        media: ["DATs_To/DATs_To001/Documentation/img1.jpg"  
+        media: ["DATs_To/DATs_To001/Documentation/img1.jpg"]  
 };
 
 /* ======================================================== */
